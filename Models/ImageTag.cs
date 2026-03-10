ab3ae3d938fc8fb9d797e7eea3011813fe5bddda
dev <Jorsg>
2026-03-09 22:18:48 -0400

    Add new models by Computer Vision

---
+++ Models/ImageTag.cs
@@ -0,0 +1,9 @@
+namespace az204_image_processor.Models
+{
+    public class ImageTag
+    {
+        
+         public string Name { get; set; } = string.Empty;
+        public double Confidence { get; set; }
+    }
+}
---
+++ Models/TextLine.cs
@@ -0,0 +1,8 @@
+namespace az204_image_processor.Models
+{
+    public class TextLine
+    {
+        public string Text { get; set; } = string.Empty;
+        public double Confidence { get; set; }
+    }
+}
